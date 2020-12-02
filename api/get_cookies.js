51c4f1fa4444feac9f3e9dd6b3fa0b175704561a
Kanisorn Sutham <kanisorn.sutham@myob.com>
2020-12-02 14:50:23 +0700

    Update serverless to have no cache

--- api/get_cookies.js
+++ api/get_cookies.js
@@ -5,5 +5,6 @@ module.exports = (req, res) => {
     "C3__SameSiteNone_And_NoSecure=__value__; Max-Age=86400; Path=/; SameSite=None",
     "C4__SameSiteNotSet_And_NoSecure=__value__; Max-Age=86400; Path=/;",
   ]);
+  res.setHeader("Cache-Control", "s-maxage=1, stale-while-revalidate");
   res.send("success");
 };
--- api/set_new_cookie.js
+++ api/set_new_cookie.js
@@ -2,5 +2,6 @@ module.exports = (req, res) => {
   res.setHeader("Set-Cookie", [
     "C4__SameSiteNotSet_And_NoSecure=__value__; Max-Age=86400; Path=/; Secure; SameSite=None",
   ]);
+  res.setHeader("Cache-Control", "s-maxage=1, stale-while-revalidate");
   res.send("success");
 };
