a7de57df751f9d6c8972897ba403fd19bb052eab
Kanisorn Sutham <kanisorn.sutham@myob.com>
2020-12-02 14:24:50 +0700

    Update get_cookies.js serverless

---
+++ api/get_cookies.js
@@ -0,0 +1,9 @@
+module.exports = (req, res) => {
+  res.setHeader("Set-Cookie", [
+    "C1__SameSiteNotSet_And_Secure=__value__; Max-Age=86400; Path=/; Secure",
+    "C2__SameSiteNone_And_Secure=__value__; Max-Age=86400; Path=/; Secure; SameSite=None",
+    "C3__SameSiteNone_And_NoSecure=__value__; Max-Age=86400; Path=/; SameSite=None",
+    "C4__SameSiteNotSet_And_NoSecure=__value__; Max-Age=86400; Path=/;",
+  ]);
+  res.send("success");
+};
